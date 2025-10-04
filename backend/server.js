776096739ae56da415b2e53d04d13d0729844d01
moses lesa <94636519+mumbi123@users.noreply.github.com>
2025-10-04 20:43:33 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ['http://localhost:5173','https://busquick-frontend-final.onrender.com' ] // Allow React app to connect
+    origin: 'http://localhost:5173','https://busquick-frontend-final.onrender.com'  // Allow React app to connect
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
