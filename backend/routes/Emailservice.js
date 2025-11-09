9ec7d548a0f3771b6fdc3b69fd13adec070cc5bb
moses lesa <94636519+mumbi123@users.noreply.github.com>
2025-11-10 01:43:33 +0800

    Update Emailservice.js

--- backend/routes/Emailservice.js
+++ backend/routes/Emailservice.js
@@ -6,8 +6,8 @@ const transporter = nodemailer.createTransport({
   port: process.env.EMAIL_PORT || 587,
   secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD
+    user: 'infobusquick@gmail.com',
+    pass: 'BusQuick4433'
   }
 });
 
